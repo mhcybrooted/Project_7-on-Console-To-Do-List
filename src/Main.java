3d600c18a692459eeace80674d88f184f0a000bf
mhcybrooted <honey.app.developer@gmail.com>
2025-09-17 19:23:48 +0600

    completed

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,39 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        TaskManager taskManager = new TaskManager();
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        while (true) {
+            System.out.print("Enter command: ");
+            String command = scanner.nextLine().trim();
+
+            if (command.equals("a")) {
+                System.out.print("Message: ");
+                String message = scanner.nextLine().trim();
+                System.out.print("Date: ");
+                String date = scanner.nextLine().trim();
+                taskManager.addTask(message, date);
+            } else if (command.startsWith("v")) {
+                String param = command.substring(1);
+                if (param.matches("\\d+")) {
+                    long id = Long.parseLong(param);
+                    taskManager.viewTaskById(id);
+                } else {
+                    taskManager.viewTasksByDate(param);
+                }
+            } else if (command.startsWith("d")) {
+                String param = command.substring(1);
+                if (param.matches("\\d+")) {
+                    long id = Long.parseLong(param);
+                    taskManager.deleteTask(id);
+                } else {
+                    System.out.println("Invalid delete command. Use d<id>");
+                }
+            } else {
+                System.out.println("Invalid command. Use: a, v<id>, v<date>, d<id>");
+            }
         }
     }
 }
